526ce137be2c9248798fdf415284dbefea0fa483
Student2 <student2@iskwien.onmicrosoft.com>
2025-05-27 17:00:57 +0200

    Forward premium

--- FX_valuation_model.R
+++ FX_valuation_model.R
@@ -4,14 +4,16 @@ library(dplyr)
 library(lubridate)
 library(knitr)
 library(kableExtra)
+library(purrr)
 
 blpConnect()
 
-### Define currencies and REER tickers
+### Define list of currencies
+
 curr <- c(
   # Emerging Markets
   "INR",  # India
-  "CNY",  # China
+  "CNH",  # China
   "EGP",  # Egypt
   "ZAR",  # South Africa
   "KRW",  # South Korea
@@ -37,6 +39,14 @@ curr <- c(
   "SGD"   # Singapore
 )
 
+
+
+###############
+### 1. REER ###
+###############
+
+
+### Define REER tickers
 broad_REER <- paste0("CTTWBR", substr(curr, 1, 2), " Index")
 narrow_REER <- paste0("CTTWNR", substr(curr, 1, 2), " Index")
 
@@ -70,7 +80,7 @@ narrow_REER_df <- data.frame(
 )
 
 ### Output table
-end_date <- max(broad_REER_df$Date)  # most recent available
+end_date <- Sys.Date()  # most recent available
 reference_dates <- c(
   "Today" = end_date,
   "t-1W" = end_date - weeks(1),
@@ -123,18 +133,7 @@ narrow_output_df["Δ5Y(%)",] <- (narrow_output_df["Today",] / narrow_output_df["
 narrow_output_df <- round(narrow_output_df, 2)
 print(narrow_output_df)
 
-
-
-
-
-
-
-
-
-
-
-
-### Print formatted outputs of REER analysis ###
+### Print formatted outputs of REER analysis
 broad_formatted_df <- broad_output_df
 broad_change_rows <- rownames(broad_formatted_df)[grepl("Δ", rownames(broad_formatted_df))]
 
@@ -202,4 +201,174 @@ kable(narrow_formatted_df,
                 full_width = F,
                 position = "center") %>%
   row_spec(0, bold = TRUE, color = "white", background = "#007ACC")
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################
+### 2. FORWARD PREMIUM ###
+##########################
+
+### Define outright forwards tickers
+curr_outrt <- curr[!curr %in% c("USD", "CZK", "PLN", "CAD", "EUR", "ILS", "SEK", "CHF", "GBP", "AUD", "HKD", "SGD")]
+fwd_1M_outrt <- paste0(
+  dplyr::case_when(
+    curr_outrt == "TWD" ~ "NTN",
+    curr_outrt == "CNY" ~ "CCN",
+    curr_outrt == "ARS" ~ "APN",
+    curr_outrt == "CLP" ~ "CHN",
+    curr_outrt == "INR" ~ "IRN",
+    curr_outrt == "KRW" ~ "KWN",
+    curr_outrt == "EGP" ~ "EPN",
+    TRUE ~ curr_outrt
+  ),
+  "+1M Curncy"
+)
+
+### Define forward points tickers for currnecies with no outright forwards
+#no outright: CZK, PLN, CAD, EUR, ILS, SEK, CHF, GBP, AUD, HKD, SGD
+curr_fwd_points <- c("CZK", "PLN", "CAD", "EUR", "ILS", "SEK", "CHF", "GBP", "AUD", "HKD", "SGD") 
+fwd_1M_points <- paste0(curr_fwd_points, "1M Curncy")
+
+### Fetch forward data using Bloomberg API
+fwd_1M_outrt_data <- list()
+for (i in 1:length(fwd_1M_outrt)) {
+  fwd_1M_outrt_data[[i]] <- bdh(fwd_1M_outrt[i], "PX_LAST", start.date = as.Date("2010-01-01"), end.date = Sys.Date())  
+  }
+names(fwd_1M_outrt_data) <- curr_outrt
+
+
+fwd_1M_points_data <- list()
+for (i in 1:length(fwd_1M_points)) {
+  fwd_1M_points_data[[i]] <- bdh(fwd_1M_points[i], "PX_LAST", start.date = as.Date("2010-01-01"), end.date = Sys.Date())  
+}
+names(fwd_1M_points_data) <- curr_fwd_points
+
+
+### Melt listed data into 2 dfs
+outrt_longest_df_name <- names(fwd_1M_outrt_data)[which.max(sapply(fwd_1M_outrt_data, nrow))]
+outrt_master_dates_df <- fwd_1M_outrt_data[[outrt_longest_df_name]] %>%
+  select(date) %>%
+  distinct() %>%
+  arrange(date) # Optional: ensure dates are sorted, though they likely are.
+
+outrt_processed_list_of_dfs <- imap(fwd_1M_outrt_data, ~ {
+  .x %>%
+    rename(!!.y := PX_LAST) %>% # Dynamically set column name to currency code
+    select(date, !!.y)         # Select only date and the new currency column
+})
+
+outrt_final_combined_df <- reduce(
+  .x = outrt_processed_list_of_dfs, # The list of dataframes to join (each now has date and one currency column)
+  .f = left_join,             # The function to apply for merging
+  .init = outrt_master_dates_df,     # The initial dataframe to start with (our master set of dates)
+  by = "date"                  # The common column to join by
+)
+
+
+fwd_points_longest_df_name <- names(fwd_1M_points_data)[which.max(sapply(fwd_1M_points_data, nrow))]
+fwd_points_master_dates_df <- fwd_1M_points_data[[fwd_points_longest_df_name]] %>%
+  select(date) %>%
+  distinct() %>%
+  arrange(date) # Optional: ensure dates are sorted, though they likely are.
+
+fwd_points_processed_list_of_dfs <- imap(fwd_1M_points_data, ~ {
+  .x %>%
+    rename(!!.y := PX_LAST) %>% # Dynamically set column name to currency code
+    select(date, !!.y)         # Select only date and the new currency column
+})
+
+fwd_points_final_combined_df <- reduce(
+  .x = fwd_points_processed_list_of_dfs, # The list of dataframes to join (each now has date and one currency column)
+  .f = left_join,             # The function to apply for merging
+  .init = fwd_points_master_dates_df,     # The initial dataframe to start with (our master set of dates)
+  by = "date"                  # The common column to join by
+)
+
+### Download spot rates
+spot_fwd_points <- paste0("USD",curr_fwd_points, " Curncy")
+spot_df <- list()
+for (i in 1:length(spot_fwd_points)) {
+  spot_df[[i]] <- bdh(spot_fwd_points[i], "PX_LAST", start.date = as.Date("2010-01-01"), end.date = Sys.Date())  
+}
+names(spot_df) <- paste0(curr_fwd_points, "_SPOT")
+
+
+
+spot_longest_df_name <- names(spot_df)[which.max(sapply(spot_df, nrow))]
+spot_master_dates_df <- spot_df[[spot_longest_df_name]] %>%
+  select(date) %>%
+  distinct() %>%
+  arrange(date) # Optional: ensure dates are sorted, though they likely are.
+
+spot_processed_list_of_dfs <- imap(spot_df, ~ {
+  .x %>%
+    rename(!!.y := PX_LAST) %>% # Dynamically set column name to currency code
+    select(date, !!.y)         # Select only date and the new currency column
+})
+
+spot_final_combined_df <- reduce(
+  .x = spot_processed_list_of_dfs, # The list of dataframes to join (each now has date and one currency column)
+  .f = left_join,             # The function to apply for merging
+  .init = spot_master_dates_df,     # The initial dataframe to start with (our master set of dates)
+  by = "date"                  # The common column to join by
+)
+
+
+final_spot_fwd_points_df <- left_join(spot_final_combined_df, fwd_points_final_combined_df, by = "date")
+
+fwd_points_df <- final_spot_fwd_points_df %>% select(date)
+for (curr in curr_fwd_points){
+  fwd_points_df[curr] <- final_spot_fwd_points_df[paste0(curr,"_SPOT")]+final_spot_fwd_points_df[curr]/10000
+} 
+
+### Combine outrights with forward point rates
+fwd_1M <- left_join(fwd_points_df, outrt_final_combined_df, by = "date")
+
+### Download all spot FX rates
+curr_ex_USD <- curr[!curr %in% c("USD")]
+spot_tickers <- paste0("USD", curr_ex_USD, " Curncy")
+spot <- list()
+for (i in 1:length(spot_tickers)) {
+  spot[[i]] <- bdh(spot_tickers[i], "PX_LAST", start.date = as.Date("2010-01-01"), end.date = Sys.Date())  
+}
+names(spot) <- curr_ex_USD
+
+spot_longest_df_name <- names(spot)[which.max(sapply(spot, nrow))]
+spot_master_dates_df <- spot[[spot_longest_df_name]] %>%
+  select(date) %>%
+  distinct() %>%
+  arrange(date) # Optional: ensure dates are sorted, though they likely are.
+
+spot_processed_list_of_dfs <- imap(spot, ~ {
+  .x %>%
+    rename(!!.y := PX_LAST) %>% # Dynamically set column name to currency code
+    select(date, !!.y)         # Select only date and the new currency column
+})
+
+spot_final_combined_df <- reduce(
+  .x = spot_processed_list_of_dfs, # The list of dataframes to join (each now has date and one currency column)
+  .f = left_join,             # The function to apply for merging
+  .init = spot_master_dates_df,     # The initial dataframe to start with (our master set of dates)
+  by = "date"                  # The common column to join by
+)
+
+
+### Calculate forward premium
+merged_df <- left_join(fwd_1M, spot_final_combined_df, by = "date")
+fwd_premium_df <- merged_df %>%select(date)
+for (curr in curr_ex_USD) {
+  fwd_premium_df[curr] <- (merged_df[paste0(curr,".x")] - merged_df[paste0(curr,".y")]) / merged_df[paste0(curr,".y")] * 100
+}
+
+### Output table
 
